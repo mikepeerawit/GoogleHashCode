a94a597e49b1435ca3d1a106314b6a63f59cad97
Peerawit Chariyawongsiri <michael.chry@gmail.com>
2021-02-25 01:02:49 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def LoadData(text):
+  my_file = open(text, "r")
+  content = my_file.read()
+  content_list = content.split("\n")
+  my_file.close()
+  return content_list;
+
+datalist = LoadData("b_little_bit_of_everything.in")
+
+def GetIngredient():
+    res = []
+    for data in datalist[1:]:
+        ingredientlist = data.split(" ")
+        [res.append(x) for x in ingredientlist[1:] if x not in res]
+    return res
+
+ingredientList = GetIngredient()
+
+def GetPizzaDataList(ingredientList):
+  pizzaList = []
+  for data in datalist[1:]:
+    pizza = data.split(" ")
+    ingredientData = 0
+    for ingredient in pizza[1:]:
+      ingredientData += pow(2, ingredientList.index(ingredient))
+    pizzaList.append(("{0:0" + str(len(ingredientList)) + "b}").format(ingredientData))
+    # print(("{0:0" + str(len(ingredientList)) + "b}").format(ingredientData))
+  return pizzaList
+
+pizzaList = GetPizzaDataList(ingredientList) #return list of PizzaData
