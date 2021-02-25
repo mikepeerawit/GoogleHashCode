c71e7c45291516e0500ef467b1023b134e959cf4
kokkakniphon <kokkakniphon@gmail.com>
2021-02-25 07:44:58 +0700

    * Combine data prebs and data compile

--- main.py
+++ main.py
@@ -1,16 +1,160 @@
 import numpy as np
+from collections import OrderedDict 
 
-teamCount = 4
-ingredientSize = 4
-activePizza = [0, 1, 2, 3]
-pizzaDataList = [[True, False, True, True],[False, False, False, True],[True, True, False, False],[False, False, True, False]]
+# FILE_PATH = "Inputs/Raw_DataSets/a_example"
+FILE_PATH = "Inputs/Raw_DataSets/b_little_bit_of_everything.in"
+#FILE_PATH = "Inputs/Raw_DataSets/c_many_ingredients.in"
+#FILE_PATH = "Inputs/Raw_DataSets/d_many_pizzas.in"
+#FILE_PATH = "Inputs/Raw_DataSets/e_many_teams.in"
+
+OUTPUT_TXT = "output.txt"
+
+class DataPrebs:
+    #variable member
+    #teamCount[0] is team2
+    #teamCount[1] is team3
+    #teamCount[2] is team4
+    _teamCount = []
+    #list of unique ingredient
+    _ingredientList = []
+    #index mapping of unique ingredient
+    _ingredientDict= {}
+    #list of pizza bits
+    _pizzaList = []
+    #ingredient size
+    _ingredientSize = 0
+
+
+    #public function member
+    #load file data
+    def LoadData(self, filePath):
+        file = open(filePath, "r")
+        wordList = file.read().split()
+
+        self._AssignTeamData(int(wordList[1]), int(wordList[2]), int(wordList[3]))
+
+        for i in range(4, len(wordList)):
+            self._AssignIngredientData(wordList[i])
+            self._AssignPizzaData(wordList[i])
+        
+        self._ingredientSize = len(self._ingredientList)
+
+        self._AdjustPizzaSize()
+
+        #testing ingredient list
+        """for i in range(0, len(self._ingredientList)):
+            print(self._ingredientList[i])"""
+        #testing pizza list
+        """for i in range(0, len(self._pizzaList)):
+            print(self._pizzaList[i])"""
+
+        self._ingredientList.clear()
+        self._ingredientDict.clear()
+        
+        file.close()
+
+    #reset file data
+    def ResetFile(self):
+        file = open(OUTPUT_TXT, "w")
+        file.write("")
+
+    #write file data
+    def OutputFile(self, teamNumber, pizzaList):
+        file = open(OUTPUT_TXT, "a")
+        file.write(str(teamNumber) + " ")
+        for i in range(0, len(pizzaList)):
+            file.write(str(pizzaList[i]) + " ")
+        file.close()
+
+    #finalizing output file data
+    def FinalizingOutput(self, dataSet, score, teamDelivered, pizzaDelivered, mlRecursion):
+        #read file data
+        readFile = open(OUTPUT_TXT, "r")
+        content = readFile.read()
+        #write file data
+        folderPath = "Outputs/" + "DataSet_" + dataSet + "/"
+        fileName = "Output_DataSet_" + dataSet + "_" + str(score) + "_" + teamDelivered + "_" + pizzaDelivered + "_ML_" + mlRecursion + ".txt"
+        writeFile = open(folderPath + fileName, "w")
+        writeFile.write(content)
+
+
+
+    #constructor
+    def __init__(self):
+        self.LoadData(FILE_PATH)
+
+
+
+    #getters
+    def GetTeamCount(self, teamNumber):
+        if teamNumber >= 2 and teamNumber <= 4:
+            return self._teamCount[teamNumber - 2]
+        else :
+            print("Invalid team number: " + str(teamNumber))
+            return 0
+    
+    def GetTotalTeamCount(self):
+        return sum(self._teamCount)
+
+    def GetPizzaList(self):
+        return self._pizzaList
+    
+    def GetIngredientSize(self):
+        return self._ingredientSize
+
+
+
+    #assigning private function
+    def _AssignTeamData(self, team2, team3, team4):
+        self._teamCount.append(team2)
+        self._teamCount.append(team3)
+        self._teamCount.append(team4)
+
+    def _AssignIngredientData(self, ingredient):
+        if ((ingredient.isdigit() == False) and (ingredient not in self._ingredientList)):
+            self._ingredientList.append(ingredient)
+            self._ingredientDict[ingredient] = len(self._ingredientList) - 1
+
+    def _AssignPizzaData(self, ingredient):
+        if (ingredient.isdigit() == True):
+            self._pizzaList.append(list())
+        else:
+            pizzaIndex = len(self._pizzaList) - 1
+            ingredientIndex = self._ingredientDict[ingredient]
+            if ingredientIndex >= len(self._pizzaList[pizzaIndex]):
+                for i in range(len(self._pizzaList[pizzaIndex]), ingredientIndex + 1):
+                    self._pizzaList[pizzaIndex].append(False)
+            self._pizzaList[pizzaIndex][ingredientIndex] = True
+    
+    def _AdjustPizzaSize(self):
+        for i in range(0, len(self._pizzaList)):
+            for j in range(len(self._pizzaList[i]), self._ingredientSize):
+                self._pizzaList[i].append(False)            
+
+
+dp = DataPrebs()
+
+
+# Data Compile
+
+totalTeamCount = dp.GetTotalTeamCount()
+teamCount = [dp.GetTeamCount(2), dp.GetTeamCount(3), dp.GetTeamCount(4)]
+ingredientSize = dp.GetIngredientSize()
+activePizza = []
 
 uniqueModifier = 1
 repeatModifier = 0
 combSizeModifier = 1
-combTresholdModifier = 0.9
+combTresholdModifier = 1
 
 findCombination_activePizza = activePizza
+
+def DeductTeamCount(teamNumber):
+  global teamCount
+  if teamNumber >= 2 and teamNumber <= 4:
+      teamCount[teamNumber - 2] -= 1
+  else :
+      print("Invalid team number: " + str(teamNumber))
 
 def GetDifferentScore(pizzaA, pizzaB):
   similarity = np.logical_and(pizzaA, pizzaB)
@@ -18,12 +162,12 @@ def GetDifferentScore(pizzaA, pizzaB):
 
 def GetCombinationScore(combination):
 
-  unique = pizzaDataList[combination[0]]
+  unique = dp.GetPizzaList()[combination[0]]
   for i in range(1, len(combination)):
-    unique = np.logical_or(unique, pizzaDataList[combination[i]])
-  repeat = pizzaDataList[combination[0]]
+    unique = np.logical_or(unique, dp.GetPizzaList()[combination[i]])
+  repeat = dp.GetPizzaList()[combination[0]]
   for i in range(1, len(combination)):
-    repeat = np.logical_and(repeat, pizzaDataList[combination[i]])
+    repeat = np.logical_and(repeat, dp.GetPizzaList()[combination[i]])
 
   uniqueCount = sum(unique)
   repeatCount = sum(repeat)
@@ -36,11 +180,13 @@ def GetCombinationScore(combination):
 
 def GetBestPizza(pizzaIndex):
   
+  global findCombination_activePizza
+
   bestPizza = -1
   bestScore = 0
 
   for i in findCombination_activePizza:
-    currentScore = GetDifferentScore(pizzaDataList[pizzaIndex], pizzaDataList[i])
+    currentScore = GetDifferentScore(dp.GetPizzaList()[pizzaIndex], dp.GetPizzaList()[i])
     if (bestScore < currentScore):
       bestPizza = i
       bestScore = currentScore
@@ -48,7 +194,8 @@ def GetBestPizza(pizzaIndex):
   return bestPizza
 
 def GetBestPizzaCombination(pizzaIndex):
-  findCombination_activePizza = activePizza
+  global findCombination_activePizza
+  findCombination_activePizza = list(activePizza)
 
   combination = []
   combination.append(pizzaIndex)
@@ -69,8 +216,9 @@ def GetBestPizzaCombination(pizzaIndex):
   return combination
 
 def AssignPizzaToTeam():
+  global activePizza
 
-  for i in range(teamCount):
+  for i in range(totalTeamCount):
     bestCombiantion = []
     bestScore = 0
 
@@ -78,22 +226,24 @@ def AssignPizzaToTeam():
       currentCombination = GetBestPizzaCombination(j)
       currentScore = GetCombinationScore(currentCombination)
 
-      if (len(currentCombination) > 1 and teamCount > 0):
+      if (len(currentCombination) > 1 and dp.GetTeamCount(len(currentCombination)) > 0):
         if (bestScore < currentScore or (bestScore == currentScore and len(currentCombination) < len(bestCombiantion))):
           bestScore = currentScore
           bestCombiantion = currentCombination
+
     if (len(bestCombiantion) > 0):
       print(len(bestCombiantion), " ", end='')
-      for n in bestCombiantion:
+      for n in range(len(bestCombiantion)):
         print(bestCombiantion[n], " ", end='')
+        # activePizza.remove(bestCombiantion[n])
       print()
-    
+      DeductTeamCount(len(bestCombiantion))
     if (len(activePizza) < 2):
       break
 
 def Main():
-
-  activePizza = range(len(pizzaDataList))
+  global activePizza
+  activePizza = list(range(len(dp.GetPizzaList())))
 
   AssignPizzaToTeam()
 
